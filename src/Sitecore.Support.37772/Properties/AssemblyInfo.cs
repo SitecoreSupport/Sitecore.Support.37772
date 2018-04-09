acd44e2a836396874d830ddd11a8dd2d4fdcfc36
DK\AMCH <amch@sitecore.net>
2018-04-09 14:24:34 +0300

    Commit initial code of the Sitecore.Commerce.XA.Foundation.CommerceEngine.Managers.SearchManager.SearchCatalogItemsByKeyword method

---
+++ src/Sitecore.Support.37772/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.37772")]
+[assembly: AssemblyProduct("Sitecore.Support.37772")]
+[assembly: ComVisible(false)]
