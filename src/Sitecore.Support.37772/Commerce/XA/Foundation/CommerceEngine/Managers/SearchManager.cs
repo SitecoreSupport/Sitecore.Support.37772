ae66ed2590e7dd16e1f799636cd2ccc16a0b2ddb
ArtemNizelnyk <arn@sitecore.net>
2018-08-20 13:37:25 +0300

    Upgrade code to 9.0.2

--- src/Sitecore.Support.37772/Commerce/XA/Foundation/CommerceEngine/Managers/SearchManager.cs
+++ src/Sitecore.Support.37772/Commerce/XA/Foundation/CommerceEngine/Managers/SearchManager.cs
@@ -1,29 +1,42 @@
-﻿using Sitecore.Commerce.XA.Foundation.Common;
-using Sitecore.Commerce.XA.Foundation.Common.Search;
-using Sitecore.Commerce.Engine.Connect;
-using Sitecore.Data.Items;
-using Sitecore.Diagnostics;
-using System.Collections.Generic;
-using System.Linq;
-using Sitecore.Commerce.Engine.Connect.Interfaces;
-using static Sitecore.Commerce.XA.Foundation.Common.Constants;
-using Sitecore.Commerce.Engine.Connect.Search;
-using Sitecore.Commerce.XA.Foundation.CommerceEngine.Search;
-using Sitecore.ContentSearch.Linq;
-
-namespace Sitecore.Support.Commerce.XA.Foundation.CommerceEngine.Managers
+﻿namespace Sitecore.Support.Commerce.XA.Foundation.CommerceEngine.Managers
 {
-    public class SearchManager : Sitecore.Commerce.XA.Foundation.CommerceEngine.Managers.SearchManager
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using Diagnostics;
+    using Sitecore.Commerce.Engine.Connect;
+    using Sitecore.Commerce.Engine.Connect.Interfaces;
+    using Sitecore.Commerce.Engine.Connect.Search;
+    using Sitecore.Commerce.XA.Foundation.Common;
+    using Sitecore.Commerce.XA.Foundation.Common.Context;
+    using Sitecore.Commerce.XA.Foundation.Common.Utils;
+    using Sitecore.Commerce.XA.Foundation.CommerceEngine.Search;
+    using Sitecore.Commerce.XA.Foundation.Common.Search;
+    using Sitecore.ContentSearch;
+    using Sitecore.ContentSearch.Exceptions;
+    using Sitecore.ContentSearch.Linq;
+    using Sitecore.ContentSearch.Linq.Utilities;
+    using Sitecore.ContentSearch.Security;
+    using Sitecore.ContentSearch.Utilities;
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
+    using static Sitecore.Commerce.XA.Foundation.Common.Constants;
+  public class SearchManager : Sitecore.Commerce.XA.Foundation.CommerceEngine.Managers.SearchManager
     {
-        public SearchManager([NotNull]IStorefrontContext storefrontContext)
-    : base(storefrontContext)
+        public SearchManager([NotNull] IStorefrontContext storefrontContext, [NotNull] IContext context)
+            : base(storefrontContext, context)
         {
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling")]
-        public override SearchResults SearchCatalogItemsByKeyword(string keyword, string catalogName, CommerceSearchOptions searchOptions)
+        [SuppressMessage("Microsoft.Maintainability", "CA1506:AvoidExcessiveClassCoupling")]
+        public override SearchResults SearchCatalogItemsByKeyword(string keyword, string catalogName,
+            CommerceSearchOptions searchOptions)
         {
             Assert.ArgumentNotNullOrEmpty(catalogName, "catalogName");
+            Assert.ArgumentNotNull(searchOptions, nameof(searchOptions));
 
             var returnList = new List<Item>();
             var totalPageCount = 0;
@@ -32,66 +45,39 @@ namespace Sitecore.Support.Commerce.XA.Foundation.CommerceEngine.Managers
 
             var searchManager = CommerceTypeLoader.CreateInstance<ICommerceSearchManager>();
             var searchIndex = searchManager.GetIndex(catalogName);
-            var startCategory = this.StorefrontContext.CurrentStorefront.GetStartNavigationCategory();
-            var startCategoryIsDepartmentLevel = startCategory?.ToString() == CatalogConstants.CatalogConfiguration.DepartmentsCatalogItemId;
+            var startCategoryPath = StorefrontContext.CurrentStorefront.GetStartNavigationCategoryItem().Paths.Path;
+            var catalogId = StorefrontContext.CurrentStorefront.CatalogItem.ID.ToGuid().ToString();
 
             using (var context = searchIndex.CreateSearchContext())
             {
-                IQueryable<CommerceSellableItemSearchResultItem> csSearchResults;
+                var query = context.GetQueryable<CommerceSellableItemSearchResultItem>()
+                    .Where(
+                        item => item.Name.Equals(keyword) || item.DisplayName.Equals(keyword)
+                                                          || item.Content.Contains(keyword))
+                    .Where(
+                        item => item.CommerceSearchItemType == CommerceSearchItemType.SellableItem
+                                || item.CommerceSearchItemType == CommerceSearchItemType.Category)
+                    .Where(item => item.Path.StartsWith(startCategoryPath))
+                    .Where(item => item.Language == CurrentLanguageName)
+                    .Where(item => item.ParentCatalogList.Contains(catalogId))
+                    .Where(item => item[BuiltinFields.LatestVersion].Equals("1"));
 
-                if (startCategoryIsDepartmentLevel)
-                {
-                    csSearchResults = context.GetQueryable<CommerceSellableItemSearchResultItem>()
-                       .Where(item =>
-                           item.Name.Equals(keyword) || item["_displayname"].Equals(keyword) ||
-                           item.Content.Contains(keyword))
-                       .Where(item =>
-                           item.CommerceSearchItemType == CommerceSearchItemType.SellableItem ||
-                           item.CommerceSearchItemType == CommerceSearchItemType.Category)
-
-                       // .Where(item => item.CatalogEntityId == catalogName)
-                       .Where(item => item.Language == this.CurrentLanguageName)
-                       .Select(p => new CommerceSellableItemSearchResultItem()
-                       {
-                           ItemId = p.ItemId,
-                           Uri = p.Uri
-                       });
-                }
-                else
-                {
-                    csSearchResults = context.GetQueryable<CommerceSellableItemSearchResultItem>()
-                        .Where(item =>
-                            item.Name.Equals(keyword) || item["_displayname"].Equals(keyword) ||
-                            item.Content.Contains(keyword))
-                        .Where(item =>
-                            item.CommerceSearchItemType == CommerceSearchItemType.SellableItem ||
-                            item.CommerceSearchItemType == CommerceSearchItemType.Category)
-                        .Where(item => item.CommerceAncestorIds.Contains(startCategory))
-                        // .Where(item => item.CatalogEntityId == catalogName)
-                        .Where(item => item.Language == this.CurrentLanguageName)
-                        .Select(p => new CommerceSellableItemSearchResultItem()
-                        {
-                            ItemId = p.ItemId,
-                            Uri = p.Uri
-                        });
-                }
-
-                var csSearchOptions = SearchHelper.ToCommerceServerSearchOptions(searchOptions);
-                csSearchResults = searchManager.AddSearchOptionsToQuery<CommerceSellableItemSearchResultItem>(csSearchResults, csSearchOptions);
-
-                var results = csSearchResults.GetResults();
+                var csSearchOptions = Sitecore.Commerce.XA.Foundation.CommerceEngine.Search.SearchHelper.ToCommerceServerSearchOptions(searchOptions);
+                query = searchManager.AddSearchOptionsToQuery<CommerceSellableItemSearchResultItem>(query,
+                    csSearchOptions);
+                var results = query.GetResults();
                 var response = SearchResponse.CreateFromSearchResultsItems(csSearchOptions, results);
 
                 if (response != null)
                 {
                     returnList.AddRange(response.ResponseItems);
-
                     totalProductCount = response.TotalItemCount;
                     totalPageCount = response.TotalPageCount;
-                    facets = SearchHelper.ToCommerceFacets(response.Facets);
+                    facets = Sitecore.Commerce.XA.Foundation.CommerceEngine.Search.SearchHelper.ToCommerceFacets(response.Facets);
                 }
 
-                var searchResults = new SearchResults(returnList, totalProductCount, totalPageCount, searchOptions.StartPageIndex, facets);
+                var searchResults = new SearchResults(returnList, totalProductCount, totalPageCount,
+                    searchOptions.StartPageIndex, facets);
 
                 return searchResults;
             }
